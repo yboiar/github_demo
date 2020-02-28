3714e25586648747babef3011ac11a57eee75304
Yuriy Boiar <yuraboiar@gmail.com>
2020-02-28 22:17:58 +0200

    added seasons switch case

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        String month = s.nextLine().toUpperCase();
+        Seasons season = null;
+
+        switch (month) {
+            case "DECEMBER":
+            case "JANUARY":
+            case "FEBRUARY":
+                season = Seasons.WINTER;
+            break;
+            case "MARCH":
+            case "APRIL":
+            case "MAY":
+                season = Seasons.SPRING;
+                break;
+            case "JUNE":
+            case "JULY":
+            case "AUGUST":
+                season = Seasons.SUMMER;
+                break;
+            case "SEPTEMBER":
+            case "OCTOBER":
+            case "NOVEMBER":
+                season = Seasons.AUTUMN;
+                break;
+            default:
+                System.out.println("No such month");
+                break;
+        }
+        if (season != null) System.out.println(season.toString());
+    }
+
+
+}
---
+++ src/com/company/Seasons.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public enum Seasons {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN
+}
